a2f590673a7724c3ec517838a40de7f2f0fbe203
Dominik Braun <mail@dominikbraun.io>
2021-05-26 12:19:18 +0200

    Update to release 0.6.1

--- Formula/timetrace.rb
+++ Formula/timetrace.rb
@@ -2,8 +2,8 @@ class Timetrace < Formula
   desc "Simple time tracking CLI"
   homepage "https://github.com/dominikbraun/timetrace"
   url "https://github.com/dominikbraun/timetrace/releases/latest/download/timetrace-darwin-amd64.tar.gz"
-  sha256 "15da6516d3ff80f0042f66b525fcc2f64a5b01d5b71ef922fedff2f49308d6af"
-  version "0.5.0"
+  sha256 "4bc2102de94fbb42b751cccb05973903466c1007422cfa4e3a22ff11faa72d1b"
+  version "0.6.1"
 
   def install
     bin.install "timetrace"
