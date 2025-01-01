2248e8f49906a498865ab29a8bd2bc4ea7c40d8e
Maik Toepfer <maik.toepfer@bmw.de>
2025-01-01 16:00:51 +0100

    add linux support + use explicit version for download

--- Formula/timetrace.rb
+++ Formula/timetrace.rb
@@ -1,9 +1,18 @@
 class Timetrace < Formula
   desc "Simple time tracking CLI"
   homepage "https://github.com/dominikbraun/timetrace"
-  url "https://github.com/dominikbraun/timetrace/releases/latest/download/timetrace-darwin-amd64.tar.gz"
-  sha256 "4845fc5ea28b71168035615c4d7c766b9763b52bf9516865506a221efb763051"
   version "0.14.3"
+
+  on_macos do
+    url "https://github.com/dominikbraun/timetrace/releases/download/v#{version}/timetrace-darwin-amd64.tar.gz"
+    sha256 "4845fc5ea28b71168035615c4d7c766b9763b52bf9516865506a221efb763051"
+  end
+
+  on_linux do
+    url "https://github.com/dominikbraun/timetrace/releases/download/v#{version}/timetrace-linux-amd64.tar.gz"
+    sha256 "a9d5fb7983578106ab32138ea6a5815755120cc68374140bffddab934f6a3631"
+  end
+
   def install
     bin.install "timetrace"
   end
